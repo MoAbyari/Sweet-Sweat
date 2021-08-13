b136a076dcbcb4b2d384f42ae1ec421cf2d004b3
hesstab <tabatabaee.hessam@gmail.com>
2021-08-13 21:40:41 +1000

    firebase read and write setup &  initial chat component

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -3,9 +3,9 @@ import firebase from 'firebase';
 const config = {
   apiKey: "AIzaSyCoSoltxlZVSYkrEgywOoxPGyQv1knhfmE",
   authDomain: "sweet-sweat.firebaseapp.com",
-  //databaseURL: "ADD-YOUR-DETAILS-HERE"
+  databaseURL: "https://sweet-sweat-default-rtdb.asia-southeast1.firebasedatabase.app/"
 };
 
 firebase.initializeApp(config);
 export const auth = firebase.auth;
-//export const db = firebase.database();
+export const db = firebase.database();
