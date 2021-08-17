3b5ac9285db8d27054033d1cbfa429dced5e08ae
MoAbyari <mo.abyari88@gmail.com>
2021-08-17 15:09:59 +1000

    Updated user Profile

--- src/components/EditProfile.js
+++ src/components/EditProfile.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import { fsDb } from "../services/firebase"
 import { getCurrentUser } from '../helpers/auth';
 import moment from 'moment';
-// import { doc, setDoc, Timestamp } from "../services/firebase";
 
 
 class EditProfile extends Component {
@@ -12,7 +11,8 @@ class EditProfile extends Component {
       showForm: false,
       name: '',
       DOB:'',
-      aboutme:''
+      aboutme:'',
+      user_profile:[]
     }
 
     this._renderName = this._renderName.bind(this);
@@ -20,10 +20,20 @@ class EditProfile extends Component {
     this._renderAboutMe = this._renderAboutMe.bind(this);
     this._handleSubmit = this._handleSubmit.bind(this);
   }
-
+///////////////////////////  Fetching existing User Info ///////////////////////
   componentDidMount(){
-
-  }
+        fsDb.collection('user_profiles')
+        .where('user_id', '==', getCurrentUser().uid)
+        .get()
+        .then((snapshots) => {
+          let user_profile = [];
+          snapshots.forEach((f) =>{
+            user_profile.push(f.data());
+          });
+        this.setState({user_profile: user_profile});
+        });
+      }
+///////////////////////  Set and update user info to Db ////////////////////////
 
   saveProfile(data){
     fsDb.collection("user_profiles")
@@ -33,10 +43,11 @@ class EditProfile extends Component {
       snapshots.forEach( (userProfile) => {
         fsDb.collection("user_profiles").doc(userProfile.id)
         .set({ name: data.name, DOB: data.DOB, aboutme: data.aboutme},{merge:true})
-        .then(() => {console.log("Document successfully written!");})
       })
     })
   }
+
+////////////////////////////  Form  eventHandler ///////////////////////////////
 
   _handleSubmit(event){
     event.preventDefault();
@@ -53,21 +64,23 @@ class EditProfile extends Component {
     this.setState({aboutme : event.target.value });
   }
 
-showForm(){
-  return(
-    <div>
-    <form onSubmit={this._handleSubmit}>
-      Name:<input type="text" onChange={this._renderName} />
-      DOB:<input type="date" onChange={this._renderDOB} />
-      Aboutme:<textarea type="text" onChange={this._renderAboutMe} />
-      <br/>
-      <input type="submit" value= "Save"/>
-    </form>
-    <input type="submit" value= "Cancel" onClick={() => this.setState({showForm: false}) }/>
-    </div>
-  )
-}
+/////////////////////////////////  Form Show  //////////////////////////////////
 
+  showForm(){
+    return(
+      <div>
+      <form onSubmit={this._handleSubmit}>
+        Name:<input type="text" onChange={this._renderName} value={this.state.name} />
+        DOB:<input type="date" onChange={this._renderDOB} />
+        Aboutme:<textarea type="text" onChange={this._renderAboutMe} />
+        <br/>
+        <input type="submit" value= "Save"/>
+      </form>
+      <input type="submit" value= "Cancel" onClick={() => this.setState({showForm: false}) }/>
+      </div>
+    )
+  }
+/////////////////////////////////   Render Form ////////////////////////////////
   render(){
     return(
       <div>
@@ -75,11 +88,42 @@ showForm(){
           Edit Profile
         </button>
           {this.state.showForm ? this.showForm() : null}
-        </div>
+          <div> <UserInfo info= {this.state.user_profile} /></div>
+      </div>
       )
     }
 
 }
+
+//////////////////////////////  Show existing details of user //////////////////
+class UserInfo extends Component {
+
+  fetchUserInfo = () => {
+    const userInfo = this.props.info;
+    return userInfo.map( (i) => {
+      return (
+        <div>
+          <h2>{i.name}</h2>
+          <h3>{moment(i.DOB.toDate()).format ('MMMM Do YYYY')}</h3>
+          <h4>{i.aboutme}</h4>
+        </div>
+
+      );
+    });
+  }
+
+  render(){
+
+    return(
+      <div>
+        {this.fetchUserInfo()}
+      </div>
+    )
+  }
+}
+
+
+
 
 
 
